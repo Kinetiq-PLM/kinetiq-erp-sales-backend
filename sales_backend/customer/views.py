86d8d1fabf4d9da3dad58eff728fadcb65d5f862
Rafael Torres <rafaelsebastian0908@gmail.com>
2025-03-20 22:32:51 +0800

    place corsheaders middleware on top

--- sales_backend/customer/views.py
+++ sales_backend/customer/views.py
@@ -5,3 +5,12 @@ from .serializers import *
 class CustomerViewSet(viewsets.ModelViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+
+    def create(self, request, *args, **kwargs):
+        c_serializer = CustomerSerializer(data=request.data)
+        if c_serializer.is_valid():
+            c_serializer.save()
+
+        # add customer to business partners master list
+
+        return super().create(request, *args, **kwargs)
