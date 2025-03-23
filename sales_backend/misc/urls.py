1d6bfefa4efd024522ba645147be4fa6d5064136
Rafael Torres <rafaelsebastian0908@gmail.com>
2025-03-23 15:17:19 +0800

    add employee to misc.urls

--- sales_backend/misc/urls.py
+++ sales_backend/misc/urls.py
@@ -2,8 +2,8 @@ from .admin.models import Products
 from rest_framework import viewsets
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
-from rest_framework.response import Response
 from rest_framework import serializers
+from .human_resources.models import Employees
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -12,11 +12,28 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
-class Products(viewsets.ModelViewSet):
+class ProductsViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Products.objects.all()
     serializer_class = ProductSerializer
 
 
+class EmployeesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employees
+        fields = "__all__"
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data["name"] = f"{data['first_name']} {data['last_name']}"
+        return data
+
+
+class EmployeesViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Employees.objects.all()
+    serializer_class = EmployeesSerializer
+
+
 router = DefaultRouter()
-router.register("product", Products)
+router.register("product", ProductsViewSet)
+router.register("employee", EmployeesViewSet)
 urlpatterns = [path("", include(router.urls))]
