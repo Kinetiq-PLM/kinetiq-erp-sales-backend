0fb41c906f5f55de506e360483607d0bc5691a59
Rafael Torres <rafaelsebastian0908@gmail.com>
2025-03-23 12:48:19 +0800

    add admin models for orders and invoice

--- sales_backend/invoice/admin.py
+++ sales_backend/invoice/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(SalesInvoices)
+admin.site.register(SalesInvoices, SalesInvoicesAdmin)
 admin.site.register(Payments)
 admin.site.register(Receipt)
