f922dcfb8eab787f79b1d7820b3fda8c628224f8
Rafael Torres <rafaelsebastian0908@gmail.com>
2025-03-23 13:59:15 +0800

    modify creation of customer view

--- sales_backend/customer/serializers.py
+++ sales_backend/customer/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
 from .models import *
+from misc.admin.models import BusinessPartnerMaster
 
 
 class CustomerSerializer(serializers.ModelSerializer):
+    partner = serializers.PrimaryKeyRelatedField(
+        queryset=BusinessPartnerMaster.objects.all()
+    )
+
     class Meta:
         model = Customer
         fields = "__all__"
