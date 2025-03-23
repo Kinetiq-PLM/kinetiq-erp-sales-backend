61bde3f580a34907b794d3c067f7087051b7cef1
Rafael Torres <rafaelsebastian0908@gmail.com>
2025-03-23 16:24:36 +0800

    update customer

--- sales_backend/customer/serializers.py
+++ sales_backend/customer/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import *
 from misc.admin.models import BusinessPartnerMaster
+from django.forms import model_to_dict
 
 
 class CustomerSerializer(serializers.ModelSerializer):
@@ -11,3 +12,12 @@ class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = "__all__"
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        if instance.partner:
+            data["partner"] = model_to_dict(
+                instance.partner,
+                fields=[field.name for field in BusinessPartnerMaster._meta.fields],
+            )
+        return data
