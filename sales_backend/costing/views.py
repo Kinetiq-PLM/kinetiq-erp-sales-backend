3e4ddd097db24ea57745d2e52d5b74ab8e39cc09
Rafael Torres <rafaelsebastian0908@gmail.com>
2025-04-03 18:39:12 +0800

    update productpricing ordering

--- sales_backend/costing/views.py
+++ sales_backend/costing/views.py
@@ -9,7 +9,7 @@ class SalesCostingViewSet(viewsets.ModelViewSet):
 
 
 class ProductPricingViewSet(viewsets.ModelViewSet):
-    queryset = ProductPricing.objects.all()
+    queryset = ProductPricing.objects.all().order_by("admin_product__product_name")
     serializer_class = ProductPricingSerializer
 
     def list(self, request, *args, **kwargs):
