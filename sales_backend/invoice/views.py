430a7f0361e64b114442001e2abced2a5fe857bc
Rafael Torres <rafaelsebastian0908@gmail.com>
2025-04-09 18:49:18 +0800

    fix invoice docu formatting

--- sales_backend/invoice/views.py
+++ sales_backend/invoice/views.py
@@ -368,7 +368,7 @@ class SalesInvoicesViewSet(viewsets.ModelViewSet):
             height - order_table._height - 75
             if num_pages > 1
             else (
-                order_table._height - ((len(items) - 1) * 38)
+                order_table._height - ((len(items) - 1) * 31)
                 if len(items) == 4
                 else (
                     order_table._height + 23
