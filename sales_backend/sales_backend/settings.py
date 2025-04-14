cfc6ba8fb8a5331f0e2cda853ec3b700e9b4866d
Rafael Torres <rafaelsebastian0908@gmail.com>
2025-04-14 19:06:36 +0800

    update db file

--- sales_backend/sales_backend/settings.py
+++ sales_backend/sales_backend/settings.py
@@ -102,31 +102,31 @@ WSGI_APPLICATION = "sales_backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "kinetiq_revised",
-#         "USER": "postgres",
-#         "PASSWORD": "123",
-#         "HOST": "localhost",
-#         "PORT": "5432",
-#         # "OPTIONS": {"options": "--search-path=sales"},
-#     }
-# }
-
-import os
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME", "Kinetiq-DB"),
-        "USER": os.getenv("DB_USER", "postgres"),
-        "PASSWORD": os.getenv("DB_PASSWORD", "KntBg3jIY0DbpH8G9bwt"),
-        "HOST": os.getenv("DB_HOST", "127.0.0.1"),
-        "PORT": os.getenv("DB_PORT", "15432"),
+        "NAME": "localdb",
+        "USER": "postgres",
+        "PASSWORD": "123",
+        "HOST": "localhost",
+        "PORT": "5432",
         # "OPTIONS": {"options": "--search-path=sales"},
     }
 }
+
+# import os
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": os.getenv("DB_NAME", "Kinetiq-DB"),
+#         "USER": os.getenv("DB_USER", "postgres"),
+#         "PASSWORD": os.getenv("DB_PASSWORD", "KntBg3jIY0DbpH8G9bwt"),
+#         "HOST": os.getenv("DB_HOST", "127.0.0.1"),
+#         "PORT": os.getenv("DB_PORT", "15432"),
+#         # "OPTIONS": {"options": "--search-path=sales"},
+#     }
+# }
 
 
 # Password validation
