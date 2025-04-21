c11823eaaee88c282e67113b3ae2b86331440e62
Rafael Torres <rafaelsebastian0908@gmail.com>
2025-04-21 11:47:12 +0800

    delivery note update

--- sales_backend/delivery/serializers.py
+++ sales_backend/delivery/serializers.py
@@ -3,12 +3,14 @@ from rest_framework import serializers
 from order.serializers import *
 from django.shortcuts import get_object_or_404
 from misc.distribution.models import ShippingCost
+from itertools import zip_longest
 
 
 class DeliveryNoteSerializer(serializers.ModelSerializer):
     order = serializers.PrimaryKeyRelatedField(queryset=Order.objects.all())
     statement = serializers.PrimaryKeyRelatedField(queryset=Statement.objects.all())
     shipping_fee = serializers.SerializerMethodField()
+    order_fulfillment = serializers.SerializerMethodField()
 
     class Meta:
         model = DeliveryNote
@@ -40,3 +42,14 @@ class DeliveryNoteSerializer(serializers.ModelSerializer):
         if shipping_fee < 0:
             shipping_fee = 0.0
         return shipping_fee
+
+    def get_order_fulfillment(self, instance):
+        order_items = StatementSerializer(instance.order.statement).data["items"]
+        delivery_items = StatementSerializer(instance.statement).data["items"]
+        is_partial = False
+        for order, delivery in zip_longest(order_items, delivery_items):
+            if delivery is None:
+                is_partial = True
+            elif order["quantity"] != delivery["quantity"]:
+                is_partial = True
+        return "Partial" if is_partial else "Full"
--- sales_backend/misc/human_resources/models.py
+++ sales_backend/misc/human_resources/models.py
@@ -159,7 +159,7 @@ class Employees(models.Model):
     employment_type = models.CharField(max_length=20, blank=True, null=True)
     status = models.CharField(max_length=20, blank=True, null=True)
     reports_to = models.ForeignKey(
-        "self", models.DO_NOTHING, db_column="reports_to", blank=True, null=True
+        "self", models.DO_NOTHING, db_column="reports_to_id", blank=True, null=True
     )
     is_supervisor = models.BooleanField(blank=True, null=True)
     created_at = models.DateTimeField(blank=True, null=True)
